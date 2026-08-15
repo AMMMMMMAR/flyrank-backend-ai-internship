a07811c5e1cf6e21c2fc57e05466c930b46843de
Ammar <moharramammar@gmail.com>
2026-08-15 05:20:40 +0300

    Stage 0: hello server

---
+++ week-02/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def get_api_info():
+    
+    return {
+        "name": "Task API",
+        "version": "1.0",
+        "endpoints": ["/tasks"]
+    }
